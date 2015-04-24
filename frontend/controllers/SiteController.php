3af33cd4b8c50a3b4fc624e0930fbd708ef690ad
Tural Aliyev <turalaliyev@live.com>
2015-04-24 21:53:56 +0500

    Changes

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -67,7 +67,8 @@ class SiteController extends Controller
 
     public function actionIndex()
     {
-        return $this->render('index');
+        return $this->renderPartial('soon');
+        //return $this->render('index');
     }
 
     public function actionLogin()
